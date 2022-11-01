e10a972adbe9bae66637829030e0304abc6480c2
Samaresh Das <samareshmail679@gmail.com>
2022-11-01 12:20:39 +0530

    optimized for clouddflare

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import('next').NextConfig} */ const nextConfig = {
+  experimental: { runtime: "experimental-edge" },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
+module.exports = nextConfig;
 
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+module.exports = nextConfig;
